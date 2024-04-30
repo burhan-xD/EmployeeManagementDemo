eec0da9ac9888c1312e18382bb4210b16b0e3771
=burhan-xD <=burhanozsahinoglu@windowslive.com>
2024-05-01 02:46:06 +0300

    Kullanici register adimlari yapildi.

--- Server/Program.cs
+++ Server/Program.cs
@@ -1,5 +1,8 @@
 using Microsoft.EntityFrameworkCore;
 using ServerLibrary.Data;
+using ServerLibrary.Helpers;
+using ServerLibrary.Repositories.Contracts;
+using ServerLibrary.Repositories.Implementation;
 
 var builder = WebApplication.CreateBuilder(args);
 
@@ -15,6 +18,10 @@ builder.Services.AddDbContext<AppDbContext>(options =>
     options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection") ??
         throw new InvalidOperationException("Sorry, your connection is not found_xD"));
 });
+
+builder.Services.Configure<JwtSection>(builder.Configuration.GetSection("JwtSection"));
+builder.Services.AddScoped<IUserAccount, UserAccountRepository>();
+
 
 var app = builder.Build();
 
